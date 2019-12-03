07b8b431a7de10ea3f3eb7584a69fd8da61c12d7
Daylend <Daylend@users.noreply.github.com>
2019-12-03 00:12:43 -0700

    Added more session logic and packet functionality

--- Session.py
+++ Session.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 
 import packet
-
+import socket
+from collections import deque
 
 # Implements the session logic using packets
 class Session:
 
     def __init__(self, dest, port):
-        self.destination = dest
-        self.port = port
-        self.states = {
+        self.address = (dest, port)
+        self.__states = {
             "CLIENT_CONNECT_SYN",
             "SERVER_CONNECT_SYN_ACK",
             "CLIENT_CONNECT_ACK",
@@ -18,25 +18,71 @@ class Session:
             "SERVER_FIN",
             "CLIENT_FIN_ACK"
         }
-        self.window = {}
+        self.__window = deque()
+        self.__socket = self.__socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.__buffsize = 4096
+        # Do I really need this?
+        self.__established = False
 
     def connect(self):
         pass
 
-    def connect_syn(self):
+    # Attempt to 3-way handshake. Will give up if anything goes wrong.
+    def __handshake(self):
+        # SEQ packet
+        seqpacket = packet.Packet()
+        seqpacket.addflag("SYN")
+        # We'll use this later
+        seqlen = seqpacket.length
+        seqpacket.setsegment("SEQ", seqpacket.sizes["SEQ"], seqlen)
+
+        try:
+            self.__socket.sendto(seqpacket.header, self.address)
+            data, address = self.__socket.recvfrom(self.__buffsize)
+
+            if data is not None:
+                datapkt = packet.Packet()
+                # Convert data string to packet
+                datapkt.fromstring(data)
+
+                # Confirm it's actually the packet we want
+                if datapkt.getflag("ACK") and datapkt.getflag("SYN"):
+                    # Make sure it's actually an ACK for our SYN
+                    if datapkt.getsegment("ACK") == seqlen:
+                        # Alright send the ack ack ack ack ack
+                        ackpacket = packet.Packet()
+                        ackpacket.addflag("ACK")
+                        # New SEQ is old ACK + new SEQ len
+                        acklen = seqlen + ackpacket.length
+                        ackpacket.setsegment("SEQ", seqpacket.sizes["SEQ"], acklen)
+                        self.__socket.sendto(datapkt.header)
+                        self.__established = True
+
+
+        except:
+            print("AHHHHHHHHHH")
+
+
+    # Step 4, receive the data from server
+    def __data_recv(self):
         pass
 
-    def connect_recv_syn_ack(self):
+    # Step 5, send ack for each data packet from server
+    def __data_ack(self):
         pass
 
-    def connect_ack(self):
+    # Step 6, receive fin from server
+    def __connect_fin(self):
         pass
 
-    def data_recv(self):
+    # Step 7, send ack from fin to server
+    def __connect_fin_ack(self):
         pass
 
-    def data_ack(self):
+    # Force disconnect from session
+    def disconnect(self):
         pass
+
 
 
 
--- packet.py
+++ packet.py
@@ -36,6 +36,12 @@ class Packet:
 
         self.header = bytearray(self.offsets["DATA"] + self.sizes["DATA"])
         self.flags = bytes(1)
+        # Length in bytes
+        self.length = self.offsets["DATA"]
+
+    # Update length field (in bytes)
+    def updatelen(self):
+        self.length = self.offsets["DATA"] + self.sizes["DATA"]
 
     # Add flag and add to header
     def addflag(self, flagname):
@@ -46,7 +52,7 @@ class Packet:
 
     # Delete flag and remove from header
     def delflag(self, flagname):
-        flagbit = self.flagnames.get(flagname)
+        flagbit = self.flagnames[flagname]
         if flagbit is not None:
             if flagbit & self.flags == 1:
                 self.flags = self.flags ^ flagbit
@@ -56,11 +62,19 @@ class Packet:
     def setflag(self, byte):
         self.setsegment("FLAGS", 1, self.flags)
 
+    def getflag(self, flagname):
+        flagbit = self.flagnames[flagname]
+        if flagbit is not None:
+            return flagbit & int(self.flags) == 1
+
+
     # Set segment of the header based on offset and size. Size (bytes) is a required field for data segment.
     def setsegment(self, segname, size, data):
         if segname is not None:
+            # Offset of the segment being updated
             offset = self.offsets.get(segname)
 
+            # If DATA, change DATA size to match new payload
             if segname == "DATA":
                 self.sizes["DATA"] = size
 
@@ -69,9 +83,9 @@ class Packet:
                 bdata = bytearray(size)
                 bdata = data.to_bytes(size, "big")
 
-                # New header byte array
-                newheaderlen = self.offsets["DATA"] + self.sizes["DATA"]
-                newheader = bytearray(newheaderlen)
+                # New header byte array, update len to fit new data size
+                self.updatelen()
+                newheader = bytearray(self.length)
 
                 for i in range(len(self.header)):
                     newheader[i] = self.header[i]
@@ -89,4 +103,25 @@ class Packet:
 
             if offset is not None and size is not None:
                 return self.header[offset:offset+size]
+
+    def fromstring(self, data):
+        # Copy entire buffer to packet temporarily
+        self.header = data
+        # Get the length of the actual buffer
+        length = self.getsegment("LENGTH")
+        # Get the length of the data field
+        datalen = length - self.offsets["DATA"]
+
+        # Set the header to the proper length
+        self.header = data.to_bytes(length, "big")
+        # Set the data size to the proper length
+        self.sizes["DATA"] = datalen
+        # Update the length field to match the new packet size
+        self.updatelen()
+        # Fix the internal flag value
+        self.flags = self.getsegment("FLAGS")
+
+
+
+
 
