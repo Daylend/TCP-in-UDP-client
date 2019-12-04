78535f99d505970a81b7335ef15df6669300e4db
Daylend <Daylend@users.noreply.github.com>
2019-12-04 15:27:52 -0700

    Added more session logic. Changed from deque to ordered dictionary

--- Session.py
+++ Session.py
@@ -2,7 +2,7 @@
 
 import packet
 import socket
-from collections import deque
+from collections import OrderedDict
 
 # Implements the session logic using packets
 class Session:
@@ -18,12 +18,15 @@ class Session:
             "SERVER_FIN",
             "CLIENT_FIN_ACK"
         }
-        self.__window = deque()
+        # Ordered dict = sorted buffer window by SEQ number = no iterating through list/queue
+        self.__window = OrderedDict()
         self.__lastack = 0
         self.__socket = self.__socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.__buffsize = 4096
         # Do I really need this?
         self.__established = False
+        # Byte array or string? Does it matter really?
+        self.__databuffer = ""
 
     def connect(self):
         self.__handshake()
@@ -51,14 +54,18 @@ class Session:
                 if datapkt.getflag("ACK") and datapkt.getflag("SYN"):
                     # Make sure it's actually an ACK for our SYN
                     if datapkt.getsegment("ACK") == seqlen:
-                        self.__lastack = datapkt.getsegment("ACK")
                         # Alright send the ack ack ack ack ack
                         ackpacket = packet.Packet()
                         ackpacket.addflag("ACK")
                         # New SEQ is old ACK + new SEQ len
                         acklen = seqlen + ackpacket.length
-                        ackpacket.setsegment("SEQ", seqpacket.sizes["SEQ"], acklen)
-                        self.__socket.sendto(datapkt.header)
+                        ackpacket.setsegment("SEQ", ackpacket.sizes["SEQ"], acklen)
+                        # Ack = old SEQ
+                        ackpacket.setsegment("ACK", ackpacket.sizes["ACK"], datapkt.getsegment("SEQ"))
+
+                        self.__lastack = ackpacket.getsegment("ACK")
+
+                        self.__socket.sendto(datapkt.header, self.address)
                         self.__established = True
 
 
@@ -72,10 +79,13 @@ class Session:
                 isfin = False
                 while not isfin:
                     if len(self.__window) > 0:
-                        # Look at the stuff in the queue before we receive new data
-
-                        # Make sure to handle FIN here as well
-                        pass
+                        # Look at the stuff in the window before we receive new data
+                        for pkt in self.__window:
+                            if self.__islatestpacket(pkt):
+                                # TODO: Pull the data from the data segment, send ack, update last ack
+                                pass
+                                if pkt.getflag("FIN"):
+                                    isfin = True
 
                     # Okay now we can handle new data
                     data, address = self.__socket.recvfrom(self.__buffsize)
@@ -87,31 +97,43 @@ class Session:
                         if self.__islatestpacket(datapkt):
                             # If the incoming data is not finished
                             if not datapkt.getflag("FIN"):
-                                # Get the data from the packet and send back an ack
+                                # TODO: Get the data from the packet and send back an ack
                                 pass
 
                             else:
                                 isfin = True
                         else:
                             # Else throw it in the queue to look at later
-                            self.__window.append(datapkt)
+                            dataseq = datapkt.getsegment("SEQ")
+                            # Make sure the SEQ number is greater than the ACK we're expecting, otherwise it's a dupe
+                            # TODO: Check for last ack and resend ACK if last ack
+                            if dataseq > self.__lastack:
+                                self.__window[datapkt] = dataseq
                     if isfin:
-                        # Wrap up we're done here
+                        # TODO: Wrap up we're done here (fin was recvd)
                         pass
             except:
-                pass
+                print("AHHHHHHHHHHHHHHHHHHH")
 
     # Check to make sure this is the next SEQ # we're expecting (last == current - len)
     def __islatestpacket(self, pkt):
-        pass
+        # We have to make sure we're taking our ACK SEQ numbers into account. (45)
+        seq = pkt.getsegment("SEQ")
+        # If seq1 is 10, ack1 is 15, and seq2 is 23, we know latest packet is seq2 - seq2.length == lastack + ack.length
+        return seq - pkt.length == self.__expectedseq()
 
-    # Send ack for each data seq packet
-    def __data_ack(self, seq):
+    # We expect the next sequence number to be the last ACK + the last ACK's length + some unknown value greater than 45
+    def __expectedseq(self):
+        temp = packet.Packet()
+        return self.__lastack + temp.length
+
+    # Send ack for each received data seq packet
+    def __data_ack(self, rseq):
 
         ackpkt = packet.Packet()
         ackpkt.addflag("ACK")
-        ackpkt.setsegment("ACK", ackpkt.sizes["ACK"], seq)
-        self.__lastack = seq
+        ackpkt.setsegment("ACK", ackpkt.sizes["ACK"], rseq + ackpkt.length)
+        self.__lastack = rseq
         pass
 
     # Step 6, receive fin from server
